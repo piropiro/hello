6b8ebe8d47c08d7e0e910f4cd34ba92883bf5d33
rara <rara@192.168.11.6>
2014-05-01 12:17:01 +0900

    gradle-test

---
+++ gradle-test/src/test/java/hello/HelloTest.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hello;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+/**
+ *
+ * @author rara
+ */
+public class HelloTest {
+    
+    public HelloTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of sayHello method, of class Hello.
+     */
+    @Test
+    public void testSayHello() {
+        Hello target = new Hello();
+        assertThat(target.sayHello(), is("hello"));
+    }
+    
+}
